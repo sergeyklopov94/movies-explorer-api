ea4d872497db2e2a0b6cbb98c7758d55cbe694e8
Sergey Klopov <sergklo94@gmail.com>
2023-06-23 13:01:07 +0300

    правки

--- src/controllers/signout.js
+++ src/controllers/signout.js
@@ -2,6 +2,10 @@ const { SUCCESS_LOGOUT_MESS } = require('../utils/constants');
 
 module.exports.logout = (req, res, next) => {
   try {
+    res.cookie('jwt', token, {
+      sameSite: 'none',
+      secure: true,
+    });
     res.clearCookie('jwt').send({ message: SUCCESS_LOGOUT_MESS });
   } catch (err) {
     next(err);
