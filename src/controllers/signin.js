075d3b65b10e6d17fedb4b85823442d39491194e
Sergey Klopov <sergklo94@gmail.com>
2023-06-23 12:36:22 +0300

    правки

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -18,6 +18,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000,
         httpOnly: true,
         sameSite: 'none',
+        secure: true,
       });
       res.send({ message: SUCCESS_AUTH_MESS });
     })
