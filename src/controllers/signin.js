7c29bb5e3d3315fb99f56cabc4d80307c0b05edc
Sergey Klopov <sergklo94@gmail.com>
2023-06-23 12:34:33 +0300

    правки

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -17,7 +17,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
-        sameSite: none,
+        sameSite: 'none',
       });
       res.send({ message: SUCCESS_AUTH_MESS });
     })
