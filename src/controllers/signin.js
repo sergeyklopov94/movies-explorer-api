6b8dcb026f6697bebde8836faf744a137194b6a0
Sergey Klopov <sergklo94@gmail.com>
2023-07-05 14:30:51 +0300

    увеличить время куки

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -15,7 +15,7 @@ module.exports.login = (req, res, next) => {
         { expiresIn: '7d' },
       );
       res.cookie('jwt', token, {
-        maxAge: 3600000,
+        maxAge: 86400000,
         httpOnly: true,
         sameSite: 'none',
         secure: true,
